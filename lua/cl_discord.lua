8d028be8a14081d46bac7f681abf3a25858845e9
Lythium <62567544+Lythium4848@users.noreply.github.com>
2021-12-05 15:39:20 +0000

    Update cl_discord.lua

--- lua/cl_discord.lua
+++ lua/cl_discord.lua
@@ -1,60 +1,39 @@
--- This requires a special module to be installed before it works correctly
--- Sorry to disappoint you
 if file.Find("lua/bin/gmcl_gdiscord_*.dll", "GAME")[1] == nil then return end
 require("gdiscord")
 
--- Configuration
-local map_restrict = false -- Should a display default image be displayed if the map is not in a given list?
-local map_list = {
-    gm_flatgrass = true,
-    gm_construct = true
-}
-local image_fallback = "default"
-local discord_id = "626155559779041331"
-local refresh_time = 60
+PIXEL = PIXEL or {}
+PIXEL.RichPresenceStartTime = PIXEL.RichPresenceStartTime or os.time()
 
-local discord_start = discord_start or -1
+if game.SinglePlayer() then return end
 
 function DiscordUpdate()
-    -- Determine what type of game is being played
-    local rpc_data = {}
-    if game.SinglePlayer() then
-        rpc_data["state"] = "Singleplayer"
-    else
-        local ip = game.GetIPAddress()
-        if ip == "loopback" then
-            if GetConVar("p2p_enabled"):GetBool() then
-                rpc_data["state"] = "Peer 2 Peer"
-            else
-                rpc_data["state"] = "Local Server"
-            end
-        else
-            rpc_data["state"] = string.Replace(ip, ":27015", "")
-        end
-    end
+    local ply = LocalPlayer()
 
-    -- Determine the max number of players
+    if not IsValid(ply) then return end
+    local playerNick = ply:Nick()
+    if ply:IsAdmin() then
+        playerNick = playerNick .. " [Admin]"
+    end
+    local largeImageText = playerNick .. " | ArsenicNetwork.co"
+
+
+    local rpc_data = {}
+
+    rpc_data["details"] = string.format("%s - Playing as %s", PIXEL.FormatMoney(ply:getDarkRPVar("money")), ply:getDarkRPVar("job"))
+    rpc_data["state"] = "ArsenicNetwork.co"
+
     rpc_data["partySize"] = player.GetCount()
     rpc_data["partyMax"] = game.MaxPlayers()
-    if game.SinglePlayer() then rpc_data["partyMax"] = 0 end
 
-    -- Handle map stuff
-    -- See the config
-    rpc_data["largeImageKey"] = game.GetMap()
-    rpc_data["largeImageText"] = game.GetMap()
-    if map_restrict and not map_list[map] then
-        rpc_data["largeImageKey"] = image_fallback
-    end
-    rpc_data["details"] = GAMEMODE.Name
-    rpc_data["startTimestamp"] = discord_start
+    rpc_data["largeImageKey"] = "arsenic"
+    rpc_data["largeImageText"] = largeImageText
+
+    rpc_data["startTimestamp"] = PIXEL.RichPresenceStartTime
 
     DiscordUpdateRPC(rpc_data)
 end
 
-hook.Add("Initialize", "UpdateDiscordStatus", function()
-    discord_start = os.time()
-    DiscordRPCInitialize(discord_id)
-    DiscordUpdate()
+DiscordRPCInitialize("903716996020060200")
 
-    timer.Create("DiscordRPCTimer", refresh_time, 0, DiscordUpdate)
-end)
+timer.Create("DiscordRPCTimer", 60, 0, DiscordUpdate)
+DiscordUpdate()
